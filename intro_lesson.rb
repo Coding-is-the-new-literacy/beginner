1951394aa278f883f1158b0e4a718ff83611c8e8
Raghav Malik <raghav1987@gmail.com>
2015-04-22 12:35:21 +0530

    add simple enhancements to lesson

--- intro_lesson.rb
+++ intro_lesson.rb
@@ -7,7 +7,7 @@ this_is_a_string = "hey there"
 
 this_is_an_array = [1,2,3,4,"hello",55.0]
 
-
+#############################################################
 # in ruby, this is a method
 def count_numbers(input_array)
 	i = 0
@@ -19,6 +19,7 @@ def count_numbers(input_array)
 	return running_total
 end
 print count_numbers([1,2,3,4])==10
+#############################################################
 
 # Another way of doing this
 def count_numbers(input_array)
@@ -29,3 +30,29 @@ def count_numbers(input_array)
 	return running_total
 end
 print count_numbers([1,2,3,4])==10
+
+#############################################################
+# Some string methods
+stringer = "This is a string"
+splitter = stringer.split("")
+print splitter
+
+joiner = splitter.join("")
+print joiner
+
+#############################################################
+# You can dynamically push items to an array like this
+
+my_array = [] #declare an empty array
+my_array << "alpha"
+
+p my_array
+
+my_array << "bravo"
+
+p my_array
+
+my_array << 12
+
+p my_array
+
