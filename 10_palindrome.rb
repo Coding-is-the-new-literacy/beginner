30234f418cd0ace749aa4beede73a90a04eee146
saketmittal <saketmittal.09@gmail.com>
2015-09-03 01:08:17 +0530

    Update 10_palindrome.rb

--- 10_palindrome.rb
+++ 10_palindrome.rb
@@ -1,6 +1,17 @@
 # write a method that takes a string as input and returns true if it is a palindrome. A palindrome is a string that is the same whether written backward or forward
 
 def palindrome?(string)
+  # Recursive
+  if string.length <= 1
+    true
+  elsif string[0] != string[-1]
+    false
+  else
+    palindrome?(string[1..-2])
+  end
+  #Non-recursive
+  #string == string.reverse
+  
 end
 
 p palindrome?("malayalam")==true
